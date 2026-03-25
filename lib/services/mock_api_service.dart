a9ec37899521ce6c36cdf2b68c2e90a1a7711ffa
YasinduR <ysndrmt@gmail.com>
2026-03-25 03:38:37 +0000

    dispatch note tin save issue resolved.

--- lib/services/mock_api_service.dart
+++ lib/services/mock_api_service.dart
@@ -5,6 +5,7 @@ import 'package:myapp/contracts/mappable.dart';
 import 'package:myapp/exceptions/app_exceptions.dart';
 import 'package:myapp/models/attendance_model.dart';
 import 'package:myapp/models/dispatch_note_model.dart';
+import 'package:myapp/models/tin_model.dart';
 import 'package:myapp/models/invoice_model.dart';
 import 'package:myapp/models/receipt_model.dart';
 import 'package:myapp/models/return_request_model.dart';
@@ -893,6 +894,7 @@ class MockApiService {
         final dispatchNote = body;
         final updatedDispatchNote = dispatchNote.copyWith(
           dispatchNumber: generateDispatchNumber(),
+          tins: List<TinData>.from(dispatchNote.tins),
         );
         final isDuplicate = DummyData.savedDispatchNotes.any(
           (existing) =>
